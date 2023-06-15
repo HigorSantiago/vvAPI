6bf38f1b6da9f0c4cdd9cdb81ef15c14cdb3840e
JoseVitorNobre <josevitortinf@gmail.com>
2023-06-14 21:45:19 -0300

    Adding UTF-8 in the export function

--- src/main/java/com/example/vvAPI/service/Export.java
+++ src/main/java/com/example/vvAPI/service/Export.java
@@ -2,16 +2,17 @@ package com.example.vvAPI.service;
 
 import com.example.vvAPI.domain.CEP;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
 public class Export {
     public static void exportarParaArquivo(CEP cep, String nomeArquivo) {
 
         String conteudo = cep.toTexto();
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo, true))) {
+        try (OutputStreamWriter  writer = new OutputStreamWriter(new FileOutputStream(nomeArquivo), StandardCharsets.UTF_8)) {
             writer.write(conteudo);
             System.out.println("Arquivo exportado com sucesso: " + nomeArquivo);
         } catch (IOException e) {
